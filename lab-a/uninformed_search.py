feb076d9d57e172a2cd33a00669a98035401d14a
Dong Cao <dcaohuu18@earlham.edu>
2020-02-07 00:32:30 -0500

    bfs now checks for duplicates in the frontier

--- lab-a/uninformed_search.py
+++ lab-a/uninformed_search.py
@@ -2,14 +2,12 @@ from state_representation import FullState, DynamicState, transition_model
 from search_datastructures import Node, BfSearchTree, DfSearchTree, print_solution
 
 
-def uninformed_expand(parent_node, search_tree, maze):
+def df_expand(parent_node, search_tree, maze):
 	for action in ['N', 'E', 'S', 'W']:
 		child_state = transition_model(maze, parent_node.get_state(), action)
 		child_node = Node(child_state, parent_node, action, parent_node.get_path_cost() + 1) 
 
-		try:
-			search_tree.get_expanded_nodes()[child_node]
-		except KeyError: #not in expanded_nodes
+		if child_node not in search_tree.get_expanded_nodes():
 			search_tree.add_to_frontier(child_node)
 
 
@@ -30,7 +28,7 @@ def single_dfs(inputFile):
 	 		break 
 
 	 	df_tree.add_to_expanded_nodes(node_to_exp)
-	 	uninformed_expand(node_to_exp, df_tree, maze)
+	 	df_expand(node_to_exp, df_tree, maze)
  	
 	try:
 		print_solution(maze, goal_node)
@@ -41,7 +39,21 @@ def single_dfs(inputFile):
 	print("The number of nodes expanded is: ", len(df_tree.get_expanded_nodes()))
 
 
-from collections import deque    
+#########################################################
+
+
+from collections import deque
+
+
+def bf_expand(parent_node, search_tree, states_frontier, maze):
+	for action in ['N', 'E', 'S', 'W']:
+		child_state = transition_model(maze, parent_node.get_state(), action)
+		child_node = Node(child_state, parent_node, action, parent_node.get_path_cost() + 1) 
+
+		if (child_node not in search_tree.get_expanded_nodes()) and (child_state not in states_frontier):
+			search_tree.add_to_frontier(child_node)
+			states_frontier.add(child_state)    
+
     
 def single_bfs(inputFile):
 	full_state = FullState(inputFile)
@@ -52,16 +64,20 @@ def single_bfs(inputFile):
 	bf_tree = BfSearchTree()
 	bf_tree.add_to_frontier(root_node)
 
+	states_frontier = {root_node.get_state()} 
+	# a set of all the states in the frontier
+	# more explanation    
+
 	while bf_tree.get_frontier() != deque(): 
 	 	node_to_exp = bf_tree.deque_frontier() #node to expand
+	 	states_frontier.remove(node_to_exp.get_state())
         
 	 	if node_to_exp.get_state().goal_test():
 	 		goal_node = node_to_exp
 	 		break 
 
 	 	bf_tree.add_to_expanded_nodes(node_to_exp)
-	 	uninformed_expand(node_to_exp, bf_tree, maze) #there may be a bug around here
-	 	#print("The number of nodes expanded is: ", len(bf_tree.get_expanded_nodes()))
+	 	bf_expand(node_to_exp, bf_tree, states_frontier, maze) 
  	
 	try:
 		print_solution(maze, goal_node)
@@ -74,4 +90,4 @@ def single_bfs(inputFile):
 
 if __name__ == '__main__':
 	single_dfs('1prize-medium.txt')
-	single_bfs('1prize-medium.txt')
+	single_bfs('1prize-open.txt')
