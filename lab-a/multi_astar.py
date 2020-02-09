8620207c238ba8b09676c96eb9282c95068e4b09
Dong Cao <dcaohuu18@earlham.edu>
2020-02-08 21:46:38 -0500

    add print_multiprize_solution() and check for duplicates in the frontier

--- lab-a/multi_astar.py
+++ lab-a/multi_astar.py
@@ -1,28 +1,62 @@
 from state_representation import FullState, DynamicState, transition_model
-from search_datastructures import Node, InformedSearchTree, print_solution
+from search_datastructures import Node, InformedSearchTree
 from greedy_bf_search import single_heuristic
+from collections import deque
 
-'''
-def print_solution(maze, goal_node):
-	print()
-'''
+
+def get_visited_prizes(goal_node):
+	visited_prizes = deque()
+	child_node = goal_node
+	parent_node = goal_node.get_parent()
+
+	while parent_node is not None:
+		if child_node.get_state().get_cheese_loc() != parent_node.get_state().get_cheese_loc():
+			visited_prizes.appendleft(child_node.get_state().get_mouse_loc())
+			
+		child_node = parent_node
+		parent_node = parent_node.get_parent()
+		
+	return visited_prizes
+
+
+def print_multiprize_sol(maze, goal_node):
+	visited_prizes = get_visited_prizes(goal_node)
+	order_list = [n for n in range(10)] + [chr(c) for c in range(ord('a'), ord('z')+1)]
+
+	for i in range(len(maze)):
+		for j in range(len(maze[i])):
+			if maze[i][j] == 1:
+				print("%", end = '')
+			else:
+				if [i, j] in visited_prizes: 
+					print(order_list[visited_prizes.index([i, j])], end = '')
+				else:
+					print(' ', end = '')
+		print()
+
 
 def multi_heuristic(current_state): #return the mahattan distance to the farthest prize
 	try:
 	    return max([single_heuristic(current_state, c) for c in current_state.get_cheese_loc()])
-	except IndexError: #cheese_loc is empty, i.e. goal reached! 
+	except ValueError: #cheese_loc is empty, i.e. goal reached! 
 		return 0
 
-def multi_astar_expand(parent_node, search_tree, maze):
+
+def multi_astar_expand(parent_node, search_tree, states_frontier, maze):
 	for action in ['N', 'E', 'S', 'W']:
 		child_state = transition_model(maze, parent_node.get_state(), action)
+		if child_state in states_frontier:
+			continue
+		
 		child_path_cost = parent_node.get_path_cost() + 1
 		child_priority = child_path_cost + multi_heuristic(child_state) 
 		
 		child_node = Node(child_state, parent_node, action, child_path_cost, child_priority) 
 
-		if child_node not in search_tree.get_expanded_nodes():
+		if child_node not in search_tree.get_expanded_nodes(): 
 			search_tree.add_to_frontier(child_node)
+			states_frontier.add(child_state)
+
 
 def multi_astar(inputFile):
 	full_state = FullState(inputFile)
@@ -33,23 +67,31 @@ def multi_astar(inputFile):
 	astar_tree = InformedSearchTree()
 	astar_tree.add_to_frontier(root_node)
 
+	states_frontier = {root_node.get_state()}
+
 	while astar_tree.get_frontier() != []:
 	 	node_to_exp = astar_tree.pop_frontier_min() #node to expand
+	 	states_frontier.remove(node_to_exp.get_state())
 	 	
 	 	if node_to_exp.get_state().goal_test():
 	 		goal_node = node_to_exp
 	 		break
 
 	 	astar_tree.add_to_expanded_nodes(node_to_exp)
-	 	multi_astar_expand(node_to_exp, astar_tree, maze)
- 	
+	 	multi_astar_expand(node_to_exp, astar_tree, states_frontier, maze)
+
 	try:
-		print_solution(maze, goal_node)
+		print_multiprize_sol(maze, goal_node)
 		print("The path cost is: ", goal_node.get_path_cost())
 	except NameError: #goal_node is not defined
 		print('Goal not reached')
 
 	print("The number of nodes expanded is: ", len(astar_tree.get_expanded_nodes()))
 
+
 if __name__ == '__main__':
 	multi_astar('multiprize-tiny.txt')
+
+#suggestion: 
+	#change expanded_nodes to expanded_states?
+	#read about IDA*
