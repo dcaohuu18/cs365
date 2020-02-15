c677b7792f51cc87645af40f118f64e957b6f127
dcaohuu18 <dcaohuu18@earlham.edu>
2020-02-15 15:04:37 -0500

    add some details and comments

--- lab-a/greedy_bf_search.py
+++ lab-a/greedy_bf_search.py
@@ -1,15 +1,12 @@
 from state_representation import FullState, DynamicState, transition_model
-from search_datastructures import Node, InformedSearchTree, print_solution
+from search_datastructures import Node, InformedSearchTree, print_sin_solution, single_heuristic
 
-def single_heuristic(current_state, prize_loc):
-	current_loc = current_state.get_mouse_loc()
-	return abs(prize_loc[1] - current_loc[1]) + abs(prize_loc[0] - current_loc[0]) #manhattan distance 
 
 def greedy_expand(parent_node, search_tree, maze, prize_loc):
 	for action in ['N', 'E', 'S', 'W']:
 		child_state = transition_model(maze, parent_node.get_state(), action)
 
-		if child_state in search_tree.get_expanded_states():
+		if child_state in search_tree.get_expanded_states(): #if it's already expanded
 			continue
 
 		child_priority = single_heuristic(child_state, prize_loc)
@@ -30,18 +27,18 @@ def single_gbfs(inputFile):
 	while gbf_tree.get_frontier() != []:
 	 	node_to_exp = gbf_tree.pop_frontier_min() #node to expand
 	 	
-	 	if node_to_exp.get_state().goal_test():
+	 	if node_to_exp.get_state().goal_test(): #goal achieved
 	 		goal_node = node_to_exp
 	 		break
 
-	 	if node_to_exp.get_state() in gbf_tree.get_expanded_states():
+	 	if node_to_exp.get_state() in gbf_tree.get_expanded_states(): 
 	 		continue 
 
 	 	gbf_tree.add_to_expanded_states(node_to_exp.get_state())
 	 	greedy_expand(node_to_exp, gbf_tree, maze, prize_loc)
  	
 	try:
-		print_solution(maze, goal_node)
+		print_sin_solution(maze, goal_node)
 		print("The path cost is: ", goal_node.get_path_cost())
 	except NameError: #goal_node is not defined
 		print('Goal not reached')
@@ -49,4 +46,4 @@ def single_gbfs(inputFile):
 	print("The number of nodes expanded is: ", len(gbf_tree.get_expanded_states()))
 
 if __name__ == '__main__':
-	single_gbfs('1prize-large.txt')
+	single_gbfs('1prize-open.txt')
